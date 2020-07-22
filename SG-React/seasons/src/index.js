da715b1738997b6c2c67ed6fc0cc39148577c818
Christopher Pupo <christopherpupo@Christophers-iMac.local>
2020-07-22 11:51:28 -0400

    working on seasons project, better understanding for react class components and error handling within

---
+++ SG-React/seasons/src/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import ReactDOM, { render } from "react-dom";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null, errorMessage: '' };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // we called setState()
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => console.log(err)
+    );
+  }
+
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
