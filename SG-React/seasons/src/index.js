22f5101a1816469e91f0c00dde55f0fa4f628e97
Christopher Pupo <christopherpupo@Christophers-iMac.local>
2020-07-24 19:06:56 -0400

    created a cool accordion widget with React using hooks and functional components

--- SG-React/seasons/src/index.js
+++ SG-React/seasons/src/index.js
@@ -19,7 +19,7 @@ class App extends Component {
     );
   }
 
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
@@ -28,6 +28,14 @@ class App extends Component {
     }
 
     return <LoadingSpinner message={'waiting on user permission...'}/>
+  }
+
+  render() {
+    return (
+      <div className="border red">
+        {this.renderContent()}
+      </div>
+    )
   }
 }
 
